9034ab1bf30f544e01a8281143325c92be09a71b
SamuelKamikiMatsuoka <samuelmatsuokakamiki@gmail.com>
2025-08-28 09:19:56 -0300

    atualização EXPRESS

--- PROJETO2/index.js
+++ PROJETO2/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express(); 
 
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Servidor de produtos rodando na porta 3000'); 
 });

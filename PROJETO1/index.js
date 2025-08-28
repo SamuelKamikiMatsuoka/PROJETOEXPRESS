025479d8d4ddc93fdcc1eb31eb58c7fc28cfd99a
SamuelKamikiMatsuoka <samuelmatsuokakamiki@gmail.com>
2025-08-28 09:21:10 -0300

    comit express

--- PROJETO1/index.js
+++ PROJETO1/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 
-app.listen(3000, () => {
+app.listen(3004, () => {
   console.log('Servidor de usuarios rodando na porta 3000');
 });
--- PROJETO3/index.js
+++ PROJETO3/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express(); 
 
 
-app.listen(3002, () => {
+app.listen(3007, () => {
   console.log('Servidor de pedidos rodando na porta 3000'); 
 });
